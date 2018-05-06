85c6ec4e5d2d10a8b6e34f41b8b439d9b5920df5
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-05-06 18:40:18 -0400

    Add files via upload

---
+++ commands/addbot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports.run = async(client, message, args) => {
+message.delete();
+if(!args[0]) return message.channel.send("Please provide your bot id!")
+if(!args[1]) return message.channel.send("Please provide a prefix!")
+if(message.guild.members.has(args[0])) return message.channel.send("**This bot is already in the server. Can't you see? -_-**")
+let channel = client.channels.get("442465425159356418");
+{
+client.fetchUser(args[0]).then(i => {
+let embed = new Discord.RichEmbed()
+.setTitle("Bot added to Queue!")
+.setDescription(`Thanks you ${message.author} for inviting your bot. It will be first tested and reviewed before [inviting](https://discordapp.com/oauth2/authorize?client_id=${args[0]}&permissions=0&scope=bot), so please be patient.`)
+.addField("Inviter", `\`${message.author.username}\``, true)
+.addField("ID", `\`${args[0]}\``, true)
+.addField("Prefix", `\`${args[1]}\``, true)
+.addField("Username", `\`${i.username}\``, true)
+.setColor(message.guild.me.displayHexColor)
+.setThumbnail(i.displayAvatarURL)
+channel.send(embed).then(msg => msg.react('⏳'))
+})
+}
+}
+module.exports.help = {
+name: "invite"
+}
