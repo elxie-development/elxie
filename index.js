ac20ac7355352a14ad9d3013cdbe427469dbd0b8
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2018-05-14 18:54:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,22 @@ fs.readdir("./commands/", (err, files) => {
            .setColor(`${message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : 0xffffff}`);
             message.channel.send(embed);
             }
-
-
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ if(command === "ping") {
+	 const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+})
 let prefix = "e.";
 let args = message.content.slice(prefix.length).trim().split(" ");
 let cmd = args.shift().toLowerCase();
